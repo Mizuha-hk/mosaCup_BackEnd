dd976348de2945d449b50dee863864b3c9e3d808
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-06-17 22:05:18 +0900

    [add]:ML Model

--- mosaCupBackendServer/EndPoints/PostEndpoints.cs
+++ mosaCupBackendServer/EndPoints/PostEndpoints.cs
@@ -5,6 +5,7 @@ using mosaCupBackendServer.Data;
 using mosaCupBackendServer.Models.DbModels;
 using mosaCupBackendServer.Models.ReqModels;
 using System.Collections.Immutable;
+using JoyLevelMLModel;
 
 namespace mosaCupBackendServer.EndPoints;
 
@@ -24,6 +25,10 @@ public static class PostEndpoints
         //add Post / return JoyLevel
         group.MapPost("/", async (PostReq reqData, mosaCupBackendServerContext db) =>
         {
+            var model = new JoyLevelModel.ModelInput();
+            model.Sentence = reqData.Content;
+            var result = JoyLevelModel.Predict(model);
+
             var post = new Post
             {
                 Id = Guid.NewGuid(),
@@ -31,11 +36,11 @@ public static class PostEndpoints
                 Content = reqData.Content,
                 PostedDate = DateTime.UtcNow,
                 ReplyId = reqData.ReplyId,
-                //JoyLevel = UseJoyModel.JudgeJoyLevel(reqData.Content)
+                JoyLevel = Convert.ToInt32(result.Avg__Readers_Joy)
             };
             db.Post.Add(post);
             await db.SaveChangesAsync();
-            return Results.Ok(0);
+            return Results.Ok(post.JoyLevel);
         })
         .WithName("CreatePost")
         .WithOpenApi();
