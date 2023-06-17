3eeb1228288330c142fae4ba92a9095119e11ac0
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-06-17 12:57:10 +0900

    [add]:Machine Leaning Model

---
+++ JoyLevelMLModel/UseJoyModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoyLevelMLModel
+{
+    public static class UseJoyModel
+    {
+        public static int JudgeJoyLevel(string Text)
+        {
+            var Data = new JoyLevelModel.ModelInput();
+            Data.Sentence = Text;
+            var result = JoyLevelModel.Predict(Data);
+
+            return Convert.ToInt32(result.Avg__Readers_Joy);
+        }
+    }
+}
