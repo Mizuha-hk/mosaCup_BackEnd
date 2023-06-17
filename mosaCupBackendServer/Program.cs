bd6094eb13568dbd4d8674854405a89c84e9541b
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-06-17 21:41:04 +0900

    [add]:connect azure key vault & SignalR Service

--- mosaCupBackendServer/Program.cs
+++ mosaCupBackendServer/Program.cs
@@ -2,8 +2,13 @@
 using Microsoft.Extensions.DependencyInjection;
 using mosaCupBackendServer.Data;
 using mosaCupBackendServer.EndPoints;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+
 builder.Services.AddDbContext<mosaCupBackendServerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("mosaCupBackendServerContext") ?? throw new InvalidOperationException("Connection string 'mosaCupBackendServerContext' not found.")));
 
